73131c098e68e2e20e7c35027636630b963c047f
Vijay <kolhalvijaysingh888@gmail.com>
2025-12-07 19:31:15 +0530

    push

---
+++ src/ex_01_Java_Basic/ex011_even_numbers.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basic;
+
+public class ex011_even_numbers
+{
+    //Print even numbers from 2 to 20
+    public static void main(String[] args)
+    {
+        for(int i=2; i<=20;i= i + 2)
+        {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/ex_01_Java_Basic/ex012.java
@@ -0,0 +1,14 @@
+package ex_01_Java_Basic;
+
+public class ex012 {
+    //Print tab;e pf 123
+    public static void main(String[] args)
+    {
+        for (int i=1;i<=10; i++ )
+        {
+            // 123 * 1=123
+            System.out.printf("123 * %d=%d\n",i,123 * i);
+
+        }
+    }
+}
