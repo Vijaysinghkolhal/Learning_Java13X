614db75e7bacbbdf4d30ed2a69cf7d878482995a
Vijay <kolhalvijaysingh888@gmail.com>
2025-12-07 16:53:02 +0530

    push

---
+++ src/ex_01_Java_Basic/ex005_table_of_9.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basic;
+
+public class ex005_table_of_9
+{
+    public static void main(String[] args)
+    {
+        for(int i = 1; i <= 10; i++) {
+            System.out.printf("9 * %d = %d\n", i, 9 * i);
+        }
+
+    }
+
+}
