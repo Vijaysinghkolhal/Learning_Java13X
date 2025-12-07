a78a7518aaee48cf952894eca25f56e4eb8a62b8
Vijay <kolhalvijaysingh888@gmail.com>
2025-12-07 19:41:44 +0530

    push

---
+++ src/ex_01_Java_Basic/ex006.java
@@ -0,0 +1,23 @@
+package ex_01_Java_Basic;
+
+public class ex006
+{
+    public static void main (String[] args)
+    {
+        int i=9;
+        {
+            System.out.printf("9* %d=%d",1,9*1);
+            System.out.printf("9* %d=%d",1,9*2);
+            System.out.printf("9* %d=%d",1,9*3);
+            System.out.printf("9* %d=%d",1,9*4);
+            System.out.printf("9* %d=%d",1,9*5);
+            System.out.printf("9* %d=%d",1,9*6);
+            System.out.printf("9* %d=%d",1,9*7);
+            System.out.printf("9* %d=%d",1,9*8);
+            System.out.printf("9* %d=%d",1,9*9);
+            System.out.printf("9* %d=%d",1,9*10);
+
+
+        }
+        }
+}
