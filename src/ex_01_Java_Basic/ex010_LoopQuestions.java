819aa07e21671998e930d1800e8416cb0a3990fa
Vijay <kolhalvijaysingh888@gmail.com>
2025-12-07 19:11:20 +0530

    push

---
+++ src/ex_01_Java_Basic/ex010_LoopQuestions.java
@@ -0,0 +1,14 @@
+package ex_01_Java_Basic;
+
+public class ex010_LoopQuestions
+{
+    //✅ 1. Print numbers from 1 to 100 without using i++
+    public static void main (String[] args)
+    {
+
+        for (int i=1; i<=100; i= i+1)
+        {
+            System.out.println(i);
+        }
+    }
+}
