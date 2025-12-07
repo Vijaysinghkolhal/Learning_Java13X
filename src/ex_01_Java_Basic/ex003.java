3eff9dbde270cb33fca3231ce74a3976780d87ae
Vijay <kolhalvijaysingh888@gmail.com>
2025-12-07 15:59:33 +0530

    push

---
+++ src/ex_01_Java_Basic/ex003.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basic;
+
+public class ex003 {
+    public static void  main(String[] args)
+    {
+        boolean is_vj_married=true;
+        System.out.println(is_vj_married);
+    }
+}
