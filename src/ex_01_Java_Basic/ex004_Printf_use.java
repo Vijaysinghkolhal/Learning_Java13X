65c7b9fe09b942f93945badea222921494bef568
Vijay <kolhalvijaysingh888@gmail.com>
2025-12-07 16:28:01 +0530

    push

---
+++ src/ex_01_Java_Basic/ex004_Printf_use.java
@@ -0,0 +1,15 @@
+package ex_01_Java_Basic;
+
+public class ex004_Printf_use
+{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+//
+        System.out.printf("%d + %d", a, b);
+
+    }
+
+
+
+}
