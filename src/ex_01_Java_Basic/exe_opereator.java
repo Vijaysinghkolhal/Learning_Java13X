d61ae85b243cb1fb393746cc9b969a1ae97bd2df
Vijay <kolhalvijaysingh888@gmail.com>
2025-12-11 23:24:29 +0530

    push

---
+++ src/ex_01_Java_Basic/exe_opereator.java
@@ -0,0 +1,24 @@
+package ex_01_Java_Basic;
+
+public class exe_opereator
+{
+ public static void main(String[] args)
+     {
+         //LOGICAL OPERATORS
+         boolean a = true;
+         System.out.println(a);
+
+         boolean b = true;
+         System.out.println(!b);
+
+            //!=not equals too
+         boolean c=true;
+         System.out.println(!!!!!c);
+
+         boolean d=true && false; //AND operator
+         System.out.println(d);
+
+         boolean e=true || false;
+         System.out.println(e);
+     }
+}
