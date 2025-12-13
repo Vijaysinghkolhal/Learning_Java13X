5af55f00384727993e8c8ccf9f419acec63fccf6
Vijay <kolhalvijaysingh888@gmail.com>
2025-12-13 18:23:28 +0530

    push

---
+++ Char/src/TernaryOperator/maxNumber.java
@@ -0,0 +1,12 @@
+package TernaryOperator;
+
+public class maxNumber
+{
+    public static void main( String[] args)
+    {
+        //Write a program to find the maximum of two numbers using ternary operator
+        int a=10, b=20;
+        int max=(a>b) ? a:b;
+        System.out.println(max);
+    }
+}
