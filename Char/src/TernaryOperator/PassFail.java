7a1e78063f40d2c63ee02184a2b843ec186a1e9b
Vijay <kolhalvijaysingh888@gmail.com>
2025-12-13 18:58:02 +0530

    push

---
+++ Char/src/TernaryOperator/PassFail.java
@@ -0,0 +1,12 @@
+package TernaryOperator;
+
+public class PassFail
+{
+    public static void main(String [] args)
+    {
+        //Check pass or fail
+        int A=45;
+        String result=(A>=40) ? "Pass":"fail";
+        System.out.println(result);
+    }
+}
