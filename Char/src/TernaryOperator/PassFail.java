5a0044920b8024f9992b7186bdfe3513e4ed1bd9
Vijay <kolhalvijaysingh888@gmail.com>
2025-12-13 19:39:28 +0530

    push

--- Char/src/TernaryOperator/PassFail.java
+++ Char/src/TernaryOperator/PassFail.java
@@ -8,5 +8,22 @@ public class PassFail
         int A=45;
         String result=(A>=40) ? "Pass":"fail";
         System.out.println(result);
+
+        //Find largest of three numbers (nested ternary)
+        int a = 190, b = 125, c = 915;
+        int largest=(a>b)? (a>c?a:c):(b>c?b:c);
+        System.out.println(largest);
+
+        //Find smallest of three numbers (nested ternary)
+        int a1=134, b1=41, c1=67;
+        int smallest=(a1<b1)?(a1<c1?a1:c1):(b1<c1?b1:c1);
+        System.out.println(smallest);
+
+        int num = -5;
+
+        String result1= (num > 0) ? "Positive": (num < 0) ? "Negative" : "Zero";
+
+        System.out.println(result1);
+
     }
 }
