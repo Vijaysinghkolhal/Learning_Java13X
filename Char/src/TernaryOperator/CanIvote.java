543d80dbc061d72a869f301feaed6b759a28afc9
Vijay <kolhalvijaysingh888@gmail.com>
2025-12-13 18:16:45 +0530

    push

---
+++ Char/src/TernaryOperator/CanIvote.java
@@ -0,0 +1,10 @@
+package TernaryOperator;
+
+public class CanIvote {
+    public static void main(String[] args) {
+        int age = 10;
+        String CanIvote = age > 18 ? "Yes" : "No";
+        System.out.println(CanIvote);
+    }
+}
+
